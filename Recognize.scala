f8aea3102afbdbce95df9156146a304f4d6f16ec
Brian Blaine <briandblaine@gmail.com>
2013-08-10 15:54:56 -0700

    Added code to calculate nearest neighbors using weighted distances.
    Added benchmark method and Distance case class

--- Recognize.scala
+++ Recognize.scala
@@ -1,6 +1,7 @@
 import scala.io.Source
 
 case class Digit(label: Int, pixels: List[Int])
+case class Distance(label: Int, value: Double)
 
 class Recognize{
 
@@ -18,20 +19,27 @@ class Recognize{
   }
 
   def OneNNClassify(sample: Digit): Int = {
-    val distances = training.map(trainee => (trainee.label, distance(sample.pixels, trainee.pixels)))
-    val nearestNeighbors = distances.sortBy(distance => distance._2).head
-    val classification = nearestNeighbors._1
-    classification
+    val distances = training.map(trainee => Distance(trainee.label, distance(sample.pixels, trainee.pixels)))
+    val nearestNeighbors = distances.sortBy(distance => distance.value).head
+    val classification = nearestNeighbors.value
+    classification.toInt
   }
 
   def KNNClassify(sample: Digit, k: Int): Int = {
-    val distances = training.map(trainee => (trainee.label, distance(sample.pixels, trainee.pixels)))
-    val nearestNeighbors = distances.sortBy(distance => distance._2).take(k)
-    val classification = nearestNeighbors.groupBy(_._1).mapValues(_.size).maxBy(_._2)._1
+    val distances = training.map(trainee => Distance(trainee.label, distance(sample.pixels, trainee.pixels)))
+    val closestNeighbor = distances.sortBy(distance => distance.value).head.value
+    val weightedDistances = distances.map(distance => Distance(distance.label, distance.value / closestNeighbor))
+    val nearestNeighbors = weightedDistances.sortBy(distance => distance.value).take(k)
+    val classification = nearestNeighbors.groupBy(_.label).mapValues(_.size).maxBy(_._2)._1
     classification
   }
 
   def classifyAll(k: Int): List[Tuple2[Int, Int]] = {
     samples.map(sample =>  (sample.label, KNNClassify(sample, k)))
+  }
+
+  //List of (expected, actual) tuples.
+  def benchmark(classification: List[Tuple2[Int, Int]]) = {
+    classification.map(x => x._1 == x._2).filter(_ == true).size / samples.size.toFloat
   }
 }
