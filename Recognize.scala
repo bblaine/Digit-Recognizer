54fc42a636c38b4328458470ba7a6aa01689e35f
Brian Blaine <briandblaine@gmail.com>
2013-08-10 15:11:17 -0700

    Added KNN method and code cleanup

--- Recognize.scala
+++ Recognize.scala
@@ -8,14 +8,13 @@ class Recognize{
   val samples = loadFile("digitscheck.csv")
 
   def loadFile(fileName: String): List[Digit] = {
-    println("Loading file " + fileName + "...")
     val lines = Source.fromFile(fileName).getLines.toList
     val digits = lines.tail.map(line => {val splitLine = line.split(","); new Digit(splitLine.head.toInt, splitLine.tail.map(_.toInt).toList)})
     digits
   }
 
   def distance(image1: List[Int], image2: List[Int]): Double = {
-    Math.sqrt((image1, image2).zipped.toList.map(z => Math.pow(2, Math.abs(z._1 - z._2))).sum)
+    Math.sqrt((image1, image2).zipped.toList.map(point => Math.pow(2, Math.abs(point._1 - point._2))).sum)
   }
 
   def OneNNClassify(sample: Digit): Int = {
@@ -25,7 +24,14 @@ class Recognize{
     classification
   }
 
+  def KNNClassify(sample: Digit, k: Int): Int = {
+    val distances = training.map(trainee => (trainee.label, distance(sample.pixels, trainee.pixels)))
+    val nearestNeighbors = distances.sortBy(distance => distance._2).take(k)
+    val classification = nearestNeighbors.groupBy(_._1).mapValues(_.size).maxBy(_._2)._1
+    classification
+  }
+
   def classifyAll(k: Int): List[Tuple2[Int, Int]] = {
-    samples.map(sample =>  (sample.label, OneNNClassify(sample)))
+    samples.map(sample =>  (sample.label, KNNClassify(sample, k)))
   }
 }
